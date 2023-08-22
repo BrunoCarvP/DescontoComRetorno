4766a069b08a6dbdf574f1a3dab7b122e9d7e14a
2022101084 <2022101084@LAB2-014>
2023-08-22 20:12:38 -0300

    Adicionar arquivos de projeto.

---
+++ DescontoComRetorno/CalculoDesconto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescontoComRetorno
+{
+    internal class CalculoDesconto
+    {
+        public double Calculo(double valor , double desconto)
+        {
+            
+            return  valor - (valor * desconto) / 100;
+        }
+
+        
+        public override string ToString(double valor, double desconto)
+        {
+            return "valor" + valor + desconto + "igual"  ;
+        }
+    }
+}
---
+++ DescontoComRetorno/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DescontoComRetorno;
+
+Console.Write("Entre com um número: ");
+double valor = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Entre com o desconto: ");
+double desconto = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+
+double resultado;
+
+
+CalculoDesconto valor_desconto = new CalculoDesconto();
+
+resultado = valor_desconto.Calculo(valor, desconto);
+// Console.WriteLine($"Com o valor de R${valor:F2} e aplicando o desconto de {desconto:F1}% o valor final fica em R${resultado}");
+
